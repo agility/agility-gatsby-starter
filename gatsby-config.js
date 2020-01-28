3c8fc0839b55089f40a8db8c477efecee9e94c61
Rob Carling <robcarling@Robs-MacBook-Pro-2.local>
2020-01-28 15:11:54 -0500

    Updates to Agility Config to resolve 401 error

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,6 @@ const agilityConfig = {
   isPreview: process.env.AGILITY_API_ISPREVIEW
 }
 
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   siteMetadata: {
     title: "Agility CMS Gatsby Starter",
@@ -27,11 +21,24 @@ module.exports = {
         guid: agilityConfig.guid, //your Agility Content Fetch API Guid
         apiKey: agilityConfig.accessToken, //your Agility Content Fetch API Key
         isPreview: agilityConfig.isPreview, //set this to true if you are using the preview API Key
-        sharedContent: ["posts", "globalheader"], //a list of reference names you want to include in your GraphQL store
-        languages: ["en-us"], //the languages you want to include
-        channels: ["website"], //the channels you want to include
-        defaultPageTemplate: "./src/templates/AgilityPage.js", //the page template that will be used to render Agility CMS pages
-        indexPage: "/home", //If you want an Agility CMS page to be your home page (i.e. '/home' to be used as '/'), set the page path here
+        debug: true,
+        sharedContent: ["posts", "globalheader", "globalfooter"], //a list of reference names you want to include in your GraphQL store
+        languages: [{
+          // The name of the language code
+          name: "English",
+          // The actual language code set in Agility CMS
+          code: "en-us", 
+          // The name to be used in the URL path that represents the current language
+          path: "en", 
+          // The path to the Agility CMS page that you want to use as your root/home page
+          homePagePath: "/home" 
+        }], 
+        // The channels you want to include
+        channels: [{
+          // The reference name for the website channel as it is defined in Agility CMS
+          referenceName: "website"
+        }],
+        masterPageTemplate: "./src/templates/AgilityPage.js", //the page template that will be used to render Agility CMS pages
       },
     },
   ],
--- src/templates/AgilityPage.js
+++ src/templates/AgilityPage.js
@@ -26,5 +26,3 @@ export default class AgilityPage extends Component {
         );
     }
 }
-
-
