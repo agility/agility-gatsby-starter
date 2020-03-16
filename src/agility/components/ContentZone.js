9800caf2548f46c84df43d1d0d771a35b78c5889
Joel Varty <joelvarty@outlook.com>
2020-03-16 14:48:22 -0400

    fix to content zone rendering.

--- src/agility/components/ContentZone.js
+++ src/agility/components/ContentZone.js
@@ -3,34 +3,36 @@ import React from 'react';
 
 const ContentZone = ({ name, page, dynamicPageItem }) => {
 
-    const Modules = () => {
-        let modules = []
-        const modulesForThisContentZone = page.zones[name];
+	const Modules = () => {
+		let modules = []
+		const modulesForThisContentZone = page.zones[name];
 
-        if (modulesForThisContentZone === undefined) {
-            console.error(`Cannot render modules for zone "${name}". This does not appear to be a valid content zone for this page template.`)
-            return;
-        }
+		if (modulesForThisContentZone === undefined) {
+			console.error(`Cannot render modules for zone "${name}". This does not appear to be a valid content zone for this page template.`)
+			return;
+		}
 
-        modulesForThisContentZone.forEach(moduleItem => {
-            const moduleDefName = moduleItem.item.properties.definitionName;
-            const ModuleComponentToRender = require(`../../modules/${moduleDefName}.js`).default;
-            const moduleProps = {
-                key: moduleItem.item.contentID,
-                dynamicPageItem: dynamicPageItem,
-                item: moduleItem.item
-            }
+		modulesForThisContentZone.forEach(moduleItem => {
+			if (moduleItem.item) {
+				const moduleDefName = moduleItem.item.properties.definitionName;
+				const ModuleComponentToRender = require(`../../modules/${moduleDefName}.js`).default;
+				const moduleProps = {
+					key: moduleItem.item.contentID,
+					dynamicPageItem: dynamicPageItem,
+					item: moduleItem.item
+				}
 
-            if (ModuleComponentToRender) {
-                modules.push(<ModuleComponentToRender {...moduleProps} />)
-            } else {
-                console.error(`No react component found for the module "${moduleDefName}". Cannot render module.`);
-            }
-        })
+				if (ModuleComponentToRender) {
+					modules.push(<ModuleComponentToRender {...moduleProps} />)
+				} else {
+					console.error(`No react component found for the module "${moduleDefName}". Cannot render module.`);
+				}
+			}
+		})
 
-        return modules;
-    }
+		return modules;
+	}
 
-    return(<Modules />)
+	return (<Modules />)
 }
 export default ContentZone;
