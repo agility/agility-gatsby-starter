135244b118988b6bbb3d2322c7a339890e47825d
Joel Varty <joelvarty@outlook.com>
2020-10-13 15:11:18 -0400

    added default content and blank page component

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,120 @@
 const agility = require('./src/agility/utils')
 const { createRemoteFileNode } = require("gatsby-source-filesystem")
 
+let usingDefaultData = false;
+
+exports.sourceNodes = async (args, configOptions) => {
+	const { actions, createNodeId, createContentDigest, getNode, getNodes, store, cache, reporter } = args;
+	const { createNode, deleteNode, deletePage, touchNode } = actions
+
+	const existingNodes = await getNodes().filter(n => n.internal.type.indexOf(`agility`) !== -1)
+
+	if (existingNodes.length === 0) {
+
+		console.log("Agility CMS Starter => Creating Default Content")
+		usingDefaultData = true
+		//create the default agility nodes so that the project can be built with no config
+			await createNode({
+				id: createNodeId(`sitemap-0-0`),
+				parent: null,
+				children: [],
+				languageCode: "en-us",
+				pagePath: "/",
+				path: "/",
+				menuText: "x",
+				pageID: 0,
+				internal: {
+					type: "agilitySitemapNode",
+					content: "",
+					contentDigest: "defaultsitemapnode"
+				}
+			});
+
+			await createNode({
+				id: createNodeId(`agilitypage-0-0`),
+				parent: null,
+				children: [],
+				languageCode: "z",
+				itemID: 0,
+				pageJson: "",
+				internal: {
+					type: "agilitypage",
+					content: "",
+					contentDigest: "agilitypage"
+				}
+			});
+
+			await createNode({
+				id: createNodeId(`agilityitem-0-0`),
+				parent: null,
+				children: [],
+				languageCode: "z",
+				itemID: 0,
+				itemJson: "",
+				internal: {
+					type: "agilityitem",
+					content: "",
+					contentDigest: "agilityitem"
+				}
+			});
+
+			await createNode({
+				id: createNodeId(`agilityGlobalHeader-0-0`),
+				parent: null,
+				children: [],
+				languageCode: "en-us",
+				itemid: 0,
+				contentID: 0,
+				customFields: { siteName: "x"},
+				properties: { referenceName: "globalheaderx"},
+				internal: {
+					type: "agilityGlobalHeader",
+					content: "",
+					contentDigest: "agilityGlobalHeader"
+				}
+			});
+
+			await createNode({
+				id: createNodeId(`agilityPost-0-0`),
+				parent: null,
+				children: [],
+				languageCode: "en-us",
+				itemid: 0,
+				contentID: 0,
+				customFields: { title: "", details: "", image: {
+					url: "https://via.placeholder.com/350x150",
+					label: "Placeholder",
+					width: 350,
+					height: 150,
+					pixelWidth: 350,
+					pixelHeight: 150
+				}},
+				sitemapNode: {},
+				properties: { referenceName: "postsx"},
+				internal: {
+					type: "agilityPost",
+					content: "",
+					contentDigest: "agilityPost"
+				}
+			});
+
+
+	}
+}
+
+exports.createPages = async (args, configOptions) => {
+	const { graphql, actions, getNode, createNodeId, createContentDigest, store } = args;
+	const { createPage, deletePage, createNode, createRedirect, createPageDependency } = actions;
+
+	if (usingDefaultData) {
+		createPage({
+			path: `/`,
+			component: require.resolve(`./src/BlankPage.js`),
+			context: {  },
+		})
+	}
+}
+
 //gatsy-node.js
 //CREATE RESOLVERS *******************************************************************************************
 exports.createResolvers = (args) => {
@@ -12,7 +126,7 @@ exports.createResolvers = (args) => {
         agilityPost: {
             //get the sitemap node that represents this item - useful for retrieving the URL for the item
             sitemapNode: agility.getDynamicPageItemSitemapNode(),
-            
+
             //[Not Implemented]
             //if we had a linked content field for 'author', this is how we'd get the author for this post in a single GraphQl query
             //linkedContent_agilityAuthor: agility.getLinkedContentItem({ type: 'agilityAuthor', linkedContentFieldName: 'author' })
@@ -36,7 +150,7 @@ exports.onCreateNode = async ({
   }) => {
     // For all Agility nodes that have an attachment field, call createRemoteFileNode
     if (
-      node.internal.type.indexOf(`agility`)  > -1 && 
+      node.internal.type.indexOf(`agility`)  > -1 &&
       node.customFields
       && (
           node.internal.type.indexOf(`agilitypage`) == -1 &&
@@ -47,18 +161,22 @@ exports.onCreateNode = async ({
           node.internal.type.indexOf(`agilityitem`) == -1
           )
     ) {
-        const customFields = Object.keys(node.customFields);
+		const customFields = Object.keys(node.customFields);
+
         await asyncForEach(customFields, async (field) => {
-        
-            const fieldKeys = Object.keys(node.customFields[field]);
+
+			const fieldKeys = Object.keys(node.customFields[field]);
+
             if(
+				fieldKeys !== undefined &&
+				fieldKeys !== null &&
                 fieldKeys.includes(`url`) &&
                 fieldKeys.includes(`pixelHeight`) &&
                 fieldKeys.includes(`pixelWidth`) &&
                 fieldKeys.includes(`width`) &&
                 fieldKeys.includes(`height`)
             ) {
-                console.log(`found ${field} on ${node.internal.type}`)
+
                 let fileNode = await createRemoteFileNode({
                     url: node.customFields[field].url, // string that points to the URL of the image
                     parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
---
+++ src/BlankPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => {
+	return (
+		<section style={{fontFamily: 'Arial'}}>
+			<h1>No Content Found</h1>
+			<p>
+				Please double check your Environment settings or your .env files.
+			</p>
+			<p>
+				Need some help?  Check out the <a href="https://help.agilitycms.com/hc/en-us/articles/360039879872">Agility CMS - Gatsby docs.</a>
+			</p>
+		</section>
+	)
+}
